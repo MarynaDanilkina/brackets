64e8d372526ee9ceb766713e5227fa42986a5590
github-MarynaDanilkina <Danilkinamari99@gmail.com>
2022-01-28 10:38:49 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let a = 0;
+  for(i=0; i<arr.length; i++){
+    if(arr[i] === '('){
+      a+= 1
+    }else if(arr[i] === ')'){
+      a+= -1
+    } 
+    
+
+  
+  if(a > 0 || a < 0){
+    return false
+  }else if(a === 0){
+    return true
+  }
+ 
+}
 }
